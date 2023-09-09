c4e4959fe3daf2baa12bfe196419c13869f4fbb4
Pravee2005 <144505086+Pravee2005@users.noreply.github.com>
2023-09-09 09:21:35 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,10 @@
+def factorial(n):
+  if n == 0 or n == 1:
+    return (1)
+  else:
+    return (n * (n - 1))
+
+
+n = int(input("enter a number"))
+fact = factorial(n)
+print("THE FACTORIAL OF", n, "IS", fact)
